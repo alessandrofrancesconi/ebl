f73c60fd7d6cbace0b66270d51fd86cac6dbceb8
Alessandro Francesconi <alessandrofrancesconi@live.it>
2015-09-18 01:03:46 +0200

    Removed JS map

--- ebl/js_src/Gruntfile.js
+++ ebl/js_src/Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
         uglify: {
             options: {
                 banner: '/*! <%= pkg.name %> - v<%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd") %>) */\n',
-                sourceMap: true
+                sourceMap: false
             },
             build: {
                 files: {
